c9aaef963e4c82235a78e12dc84970108c891836
Munia Elayyan <MuniaElayyan2024@gmail.com>
2026-09-04 21:03:41 +0300

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import "./index.css"
 import App from "./App"
 import { BrowserRouter } from "react-router-dom"
 
+const basename = import.meta.env.BASE_URL.replace(/\/$/, "")
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename || undefined}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
