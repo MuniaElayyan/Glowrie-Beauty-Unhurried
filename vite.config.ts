9c8af3192732d5c50e500d5a18b78d11bd293d3d
Munia Elayyan <MuniaElayyan2024@gmail.com>
2026-09-04 21:23:40 +0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "build" ? "/Glowrie-Beauty-Unhurried/" : "/",
-}))
+
+  base:
+    command === "build"
+      ? "/Glowrie-Beauty-Unhurried/"
+      : "/",
+}));
