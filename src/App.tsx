0c648c1697dd074fbe4b79a9664c5ff5195916b2
Munia Elayyan <MuniaElayyan2024@gmail.com>
2026-09-04 23:49:57 +0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,25 @@ import { About } from "./pages/About"
 import { Navbar } from "./components/Navbar"
 import { Footer } from "./components/Footer"
 import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+import { ThemeProvider } from "./context/ThemeContext"
 
 function App() {
     return (
-        <ShoppingCartProvider>
-            <div className="min-h-screen flex flex-col font-body text-plum-600">
-                <Navbar />
-                <div className="max-w-6xl mx-auto px-5 w-full flex-1">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/store" element={<Store />} />
-                        <Route path="/about" element={<About />} />
-                    </Routes>
+        <ThemeProvider>
+            <ShoppingCartProvider>
+                <div className="min-h-screen flex flex-col font-body text-plum-600 bg-white dark:bg-wine-950 dark:text-sand-200 transition-colors duration-300">
+                    <Navbar />
+                    <div className="max-w-6xl mx-auto px-5 w-full flex-1">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/store" element={<Store />} />
+                            <Route path="/about" element={<About />} />
+                        </Routes>
+                    </div>
+                    <Footer />
                 </div>
-                <Footer />
-            </div>
-        </ShoppingCartProvider>
+            </ShoppingCartProvider>
+        </ThemeProvider>
     )
 }
 
